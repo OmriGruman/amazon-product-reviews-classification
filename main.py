6ac3b689a0a68831d64dccf8dd7609740cb78603
Mark Klaiman <markkl1998@gmail.com>
2022-12-08 19:09:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
-from sklearn.svm import SVC
-from sklearn.feature_extraction.text import CountVectorizer
-
 def classify(train_data, test_data):
-    # Preprocess the train and test data to extract the review text and ratings
-    train_text, train_labels = preprocess_data(train_data)
-    test_text, test_labels = preprocess_data(test_data)
+    # Create feature vectors using CountVectorizer or TfidfVectorizer
+    vectorizer = CountVectorizer(max_features=1000)
+    X_train = vectorizer.fit_transform([review['reviewText'] for review in train_data])
+    X_test = vectorizer.transform([review['reviewText'] for review in test_data])
     
-    # Convert the text data into a matrix of token counts
-    vectorizer = CountVectorizer()
-    train_vectors = vectorizer.fit_transform(train_text)
-    test_vectors = vectorizer.transform(test_text)
+    # Extract the 15 features with the highest discriminative power
+    selector = SelectKBest(k=15)
+    X_train = selector.fit_transform(X_train, [review['overall'] for review in train_data])
+    X_test = selector.transform(X_test)
     
-    # Train an SVM model on the training data
-    svm = SVC()
-    svm.fit(train_vectors, train_labels)
+    # Train a classifier, such as a LinearSVC or a random forest classifier, on the train data
+    clf = LinearSVC()
+    clf.fit(X_train, [review['overall'] for review in train_data])
     
-    # Use the trained model to make predictions on the test data
-    predictions = svm.predict(test_vectors)
+    # Use the trained classifier to make predictions on the test data
+    y_pred = clf.predict(X_test)
     
-    # Compute the F1 score and overall accuracy for each class
-    f1_scores = {}
-    for label in set(test_labels):
-        f1 = f1_score(test_labels, predictions, pos_label=label)
-        f1_scores[label] = f1
+    # Evaluate the classifier's performance using the F1 metric and overall accuracy
+    f1 = f1_score(y_pred, [review['overall'] for review in test_data], average=None)
+    acc = accuracy_score(y_pred, [review['overall'] for review in test_data])
     
-    overall_accuracy = accuracy_score(test_labels, predictions)
+    # Print the confusion matrix
+    print(confusion_matrix(y_pred, [review['overall'] for review in test_data]))
     
-    # Return the F1 scores and overall accuracy as a dictionary
-    return {'F1 scores': f1_scores, 'Overall accuracy': overall_accuracy}
-    
-    
+    # Return the F1 metric and overall accuracy
+    return {'f1': f1, 'accuracy': acc}
